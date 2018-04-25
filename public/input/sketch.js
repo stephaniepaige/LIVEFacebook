742705777ef7db5684f0fbee45c891e51f72f4f7
stephaniepaige <31706920+stephaniepaige@users.noreply.github.com>
2018-04-25 10:21:06 -0400

    Update sketch.js

--- public/input/sketch.js
+++ public/input/sketch.js
@@ -12,7 +12,7 @@ function setup() {
 }
 
 function draw(){
-  background(0, 255, 120);
+  background(0, 0, 255);
   // noStroke();
   // fill(0);
 
@@ -21,15 +21,15 @@ function draw(){
 //   // RotationXY gives you numbers from -180 to 180.
   let lr = floor(rotationY);
   let tb = floor(rotationX);
-//   // Ignore flipped over device
-//   lr = constrain(lr, -90, 90);
-//   tb = constrain(tb, -90, 90);
-// //use abs function to map what degree of tilt matters
-//
-//   // Map rotation to alpha range
-//   let lra = map(lr, -90, 90, 255, 0)
-//   let tba = map(tb, -90, 90, 255, 0)
-//
+  // Ignore flipped over device
+  lr = constrain(lr, -90, 90);
+  tb = constrain(tb, -90, 90);
+//use abs function to map what degree of tilt matters
+
+  // Map rotation to alpha range
+  let lra = map(lr, -90, 90, 255, 0)
+  let tba = map(tb, -90, 90, 255, 0)
+
 //
 //   // Transparency reflects degree of tilt
 //   // Left Half
@@ -42,23 +42,20 @@ function draw(){
 //   fill(0, tba);
 //   rect(0, 0, width, height/2);
 //   // Bottom Half
-//   fill(0, 255-tba);
+//   fill(0, 255-tba);cd
 //   rect(0, height/2, width, height);
 
   // Send tilt angle
-  if(abs(lr) > 45) {
+  if(abs(tb) < 45) {
     socket.emit('tilt')
   }
-  //socket.emit('tilt', {x: lr, y: tb});
+  socket.emit('tilt', {x: lr, y: tb});
 
 // Calculate size of shake
 // Send data
-// function deviceShaken() {
-//   let value = abs(accelerationX-pAccelerationX) + abs(accelerationY-pAccelerationY);
-//   socket.emit('shake');
-// }
+  }
 function deviceShaken() {
-  if (value > 50) {
-    socket.emit('shake')
+  if (value > 40) {
+    socket.emit('shake');
   }
 }
